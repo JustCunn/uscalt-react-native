661dddf2677f1ee682ff54d49fcda57ed49e3eac
JustCunn <just1brill@outlook.com>
2022-01-04 17:51:14 +0000

    Removed Signin from check_links

--- src/check_links.js
+++ src/check_links.js
@@ -4,32 +4,6 @@ import { UscaltFunctions } from 'uscalt_functions.js';
 import { SHA3 } from 'sha3';
 import md5 from 'crypto-js/md5';
 
-export async function signin(username, password) {
-    // Sign a user into the Uscalt Backend and store the Auth Token securely
-
-    const response = await fetch("http://192.168.0.3:8000/login/", {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify({
-                'username': username,
-                'password': password,
-            })
-        })
-        .then(result => result.json())
-        .catch(error => console.log('error====1========:', error))
-
-        if (response.token !== null) {
-            const hash = new SHA3(512);
-            hash.update(username)
-            hash.update(response.token)
-            await SecureStore.setItemAsync('device_hash', hash.digest('hex'));
-        }
-
-        await SecureStore.setItemAsync('secure_token', response.token);
-}
-
 export async function checkLinks() {
     //Poll Uscalt for current data that's being sought after
     let links = []
